9884c4636e45909aa52af5db4d9652a69536c20b
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-08-31 14:53:51 +0200

    [feat] Formatting App and HomeScreen

--- TDDeter/App.js
+++ TDDeter/App.js
@@ -1,7 +1,7 @@
-import LoginScreen from './Screens/LoginScreen';
-import HomeScreen from './Screens/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import LoginScreen from "./Screens/LoginScreen";
+import HomeScreen from "./Screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
--- TDDeter/Screens/HomeScreen.js
+++ TDDeter/Screens/HomeScreen.js
@@ -1,21 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native';
-import Navbar from '../Components/Navbar';
+import { View, Text, StyleSheet } from "react-native";
+import Navbar from "../Components/Navbar";
 
 export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <Navbar/>
-            <Text>Home Screen</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Navbar />
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
