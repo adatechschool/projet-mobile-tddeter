ab7bf2ee05a4d271d00ba56da332bb603b30b07c
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-08-31 16:10:04 +0200

    [feat] check empty email input on submit in login screen

--- TDDeter/Screens/LoginScreen.js
+++ TDDeter/Screens/LoginScreen.js
@@ -1,12 +1,17 @@
 import { View, Text, TextInput, Button } from "react-native";
 
 export default function LoginScreen() {
-  return (
+  
+const submitForm = ()=>{
+  throw new Error("Champ email requis");   // erreur à retravailler pour l'UX
+}
+
+  return ( 
     <View>
       <Text>Login Screen</Text>
       <TextInput placeholder="Votre Email" />
       <TextInput placeholder="Votre mot de passe" />
-      <Button title="Se connecter" />
+      <Button onPress={submitForm} title="Se connecter" />
     </View>
   );
 }
