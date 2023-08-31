16f201305491909e6a375699588d991393387a96
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-08-31 14:54:35 +0200

    [test] Testing LoginScreen : rendering and email TextInput

---
+++ TDDeter/tests/LoginScreen.test.js
@@ -0,0 +1,16 @@
+import LoginScreen from "../Screens/LoginScreen";
+import { render, screen } from "@testing-library/react-native";
+
+describe("LoginScreen test suite", () => {
+  it("should render LoginScreen", () => {
+    render(<LoginScreen />);
+    const loginScreen = screen.getByText("Login Screen");
+    expect(loginScreen).toBeOnTheScreen();
+  });
+
+  it("should display email input", () => {
+    render(<LoginScreen />);
+    const emailInput = screen.getByPlaceholderText("mail", { exact: false });
+    expect(emailInput).toBeOnTheScreen();
+  });
+});
