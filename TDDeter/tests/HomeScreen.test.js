4cd533323cf98acbe4d11a4f0d6e849e2068238f
Malena Guallar <malena.guallar@gmail.com>
2023-09-04 16:39:35 +0200

    [test] Testing if Next Event component is rendered on home page

---
+++ TDDeter/tests/HomeScreen.test.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "../Screens/HomeScreen";
+import { screen, render } from "@testing-library/react-native";
+
+describe("Next event test suite", () => {
+  it("should render next event component", () => {
+    render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
+    const NextEvent = screen.getByText("prochain", { exact: false });
+    expect(NextEvent).toBeOnTheScreen();
+  });
+});
