af1cebe943a9b306d9f64f38ed91e62cfdfadf2c
Malena Guallar <malena.guallar@gmail.com>
2023-09-04 16:39:00 +0200

    [feat] Created Next Event component to display next event

---
+++ TDDeter/Components/NextEvent.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function NextEvent() {
+  return (
+    <View>
+      <Text>Prochain évènement : Les puces de Montreuil le 6 novembre.</Text>
+    </View>
+  );
+}
