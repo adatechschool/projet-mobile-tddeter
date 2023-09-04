0b5e93a9f1ce380432588c7dd35eda7fc8f86910
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-04 12:04:10 +0200

    [feat] handling redirection to home screen after correct login submission

--- TDDeter/Screens/LoginScreen.js
+++ TDDeter/Screens/LoginScreen.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function LoginScreen() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigation = useNavigation();
+  const redirectToHome = () => {
+    navigation.navigate("Home");
+  };
 
   const submitForm = () => {
     if (mail === "") {
       throw new Error("Champ email requis");
     } else if (password === "") {
       throw new Error("Champ mot de passe requis");
+    } else {
+      redirectToHome();
     }
   };
 
@@ -20,8 +28,8 @@ export default function LoginScreen() {
         placeholder="Votre Email"
         onChangeText={(newMail) => setMail(newMail)}
       />
-      <TextInput 
-        placeholder="Votre mot de passe" 
+      <TextInput
+        placeholder="Votre mot de passe"
         onChangeText={(newPassword) => setPassword(newPassword)}
       />
       <Button onPress={submitForm} title="Se connecter" />
