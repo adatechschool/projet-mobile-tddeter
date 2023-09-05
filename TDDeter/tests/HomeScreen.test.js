b6e01bb9fbf284ed543a357a2c1d613beae77825
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-09-05 10:49:51 +0200

    [test] renamed test suite to HomeScreen test suite

--- TDDeter/tests/HomeScreen.test.js
+++ TDDeter/tests/HomeScreen.test.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../Screens/HomeScreen";
 import { screen, render } from "@testing-library/react-native";
 
-describe("Next event test suite", () => {
+describe("HomeScreen test suite", () => {
   it("should render next event component", () => {
     render(
       <NavigationContainer>
@@ -12,4 +12,5 @@ describe("Next event test suite", () => {
     const NextEvent = screen.getByText("prochain", { exact: false });
     expect(NextEvent).toBeOnTheScreen();
   });
+
 });
