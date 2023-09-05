3342c16edb5e88d455a80579ba37c301abb9c443
Malena Guallar <malena.guallar@gmail.com>
2023-09-05 12:20:07 +0200

    [feat] Added onPress function to pressable element

--- TDDeter/Components/NextEvent.js
+++ TDDeter/Components/NextEvent.js
@@ -1,12 +1,12 @@
 import { View, Text, Pressable } from "react-native";
 
-export default function NextEvent({ city, date }) {
+export default function NextEvent({ city, date, onPressAction }) {
   return (
     <View>
       <Text>
         Prochaine brocante : {city}, le {date}.
       </Text>
-      <Pressable accessibilityRole="button">
+      <Pressable accessibilityRole="button" onPress={onPressAction}>
         <Text>Clique ici pour voir les infos</Text>
       </Pressable>
     </View>
