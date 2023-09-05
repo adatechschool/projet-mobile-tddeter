b2f5c5f2548942af390a5a090724f79d9dda6bfa
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-05 11:49:31 +0200

    [feat] Add a pressable element in NextEvent

--- TDDeter/Components/NextEvent.js
+++ TDDeter/Components/NextEvent.js
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 export default function NextEvent( {city, date} ) {
   return (
     <View>
       <Text>Prochaine brocante : {city}, le {date}.</Text>
+      <Pressable accessibilityRole="button"></Pressable>
     </View>
   );
 }
