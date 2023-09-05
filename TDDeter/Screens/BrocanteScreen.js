6552b5a31dccd51ed948c1d4c99b93af142d67cc
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-09-05 18:01:18 +0200

    [feat] Adding date prop on BrocanteScreen

--- TDDeter/Screens/BrocanteScreen.js
+++ TDDeter/Screens/BrocanteScreen.js
@@ -1,12 +1,13 @@
 import { View, Text } from "react-native";
 import Navbar from "../Components/Navbar";
 
-export default function BrocanteScreen({ city }) {
+export default function BrocanteScreen({ city, date }) {
   return (
     <View>
       <Navbar />
       <Text>Brocante Screen</Text>
       <Text>et les exposants voila {city}</Text>
+      <Text> Elles et ils seront là le {date}</Text>
     </View>
   );
 }
