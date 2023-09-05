7c8f2500e94c90333a47342490f05bfbbd331bdb
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-05 15:42:40 +0200

    [feat] Create BrocanteScreen with display of Navbar

--- TDDeter/Screens/BrocanteScreen.js
+++ TDDeter/Screens/BrocanteScreen.js
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
+import Navbar from "../Components/Navbar";
 
 export default function BrocanteScreen() {
   return (
     <View>
+      <Navbar />
       <Text>Brocante Screen</Text>
       <Text>et les exposants voila </Text>
     </View>
