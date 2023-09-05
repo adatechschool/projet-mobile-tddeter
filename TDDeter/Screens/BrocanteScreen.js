c85d941a74274fb6f26c99230e0f00a92c47cec8
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-05 14:26:35 +0200

    [feat] add redirection to brocante Screen on press of info in nextevent

---
+++ TDDeter/Screens/BrocanteScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function BrocanteScreen() {
+  return (
+    <View>
+      <Text>Brocante Screen</Text>
+      <Text>et les exposants voila </Text>
+    </View>
+  );
+}
