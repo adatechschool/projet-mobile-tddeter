127cbedcbcdc0cca0e1d205d8731d8249b16486a
Malena Guallar <malena.guallar@gmail.com>
2023-09-06 15:31:16 +0200

    [feat] Added name as a prop inside Exposant component

--- TDDeter/Components/Exposant.js
+++ TDDeter/Components/Exposant.js
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 
-export default function Exposant() {
+export default function Exposant({ name }) {
   return (
     <View accessibilityLabel="exposant">
       <Text> Ceci est un brocanteur</Text>
+      <Text>Son nom est {name}</Text>
     </View>
   );
 }
