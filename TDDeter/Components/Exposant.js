1705ed417e1673230a352e8117089566bfbda43b
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-06 15:08:54 +0200

    [feat] adding new component exposant to brocante screen

---
+++ TDDeter/Components/Exposant.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Exposant() {
+  return (
+    <View accessibilityLabel="exposant">
+      <Text> Ceci est un brocanteur</Text>
+    </View>
+  );
+}
--- TDDeter/Screens/BrocanteScreen.js
+++ TDDeter/Screens/BrocanteScreen.js
@@ -1,5 +1,6 @@
 import { View, Text } from "react-native";
 import Navbar from "../Components/Navbar";
+import Exposant from "../Components/Exposant";
 
 export default function BrocanteScreen({ city, date, openingTimes }) {
   return (
@@ -9,6 +10,7 @@ export default function BrocanteScreen({ city, date, openingTimes }) {
       <Text>Venez voir les exposant.e.s de la brocante de {city}</Text>
       <Text>Elles et ils seront là le {date}</Text>
       <Text>Horaires : {openingTimes}</Text>
+      <Exposant/>
     </View>
   );
 }
