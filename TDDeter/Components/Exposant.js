4985416b6d9a8360f64edca65cbe0424ca4a43dd
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-06 16:29:46 +0200

    [feat] add catgories to exposant commponent

--- TDDeter/Components/Exposant.js
+++ TDDeter/Components/Exposant.js
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
 
-export default function Exposant({ name, biography }) {
+export default function Exposant({ name, biography, categories }) {
   return (
     <View accessibilityLabel="exposant">
       <Text> Ceci est un brocanteur</Text>
       <Text>Son nom est {name}</Text>
       <Text>Bio : {biography}</Text>
+      <Text>Catégories : {categories}</Text>
     </View>
   );
 }
