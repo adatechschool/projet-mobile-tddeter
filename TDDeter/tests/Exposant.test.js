358c6bb74095e3d9c71887f0dad62ba47762d858
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-06 16:12:02 +0200

    [test] Test biography passed as a prop is displayed for Exposant

--- TDDeter/tests/Exposant.test.js
+++ TDDeter/tests/Exposant.test.js
@@ -1,19 +1,32 @@
 import Exposant from "../Components/Exposant";
 import { render, screen } from "@testing-library/react-native";
 
-
 describe("Exposant component test suite", () => {
-    it("should display exposant's name", () => {
-        const name = "Lulu la brocante";
-        render(<Exposant name =  {name} />);
-        const exposantName = screen.getByText(name, {exact: false});
-        expect(exposantName).toBeOnTheScreen();
-    });
+  it("should display exposant's name", () => {
+    const name = "Lulu la brocante";
+    render(<Exposant name={name} />);
+    const exposantName = screen.getByText(name, { exact: false });
+    expect(exposantName).toBeOnTheScreen();
+  });
 
-    it("should display exposant's name as a prop", () => {
-        const name = "Benji la malle";
-        render(<Exposant name =  {name} />);
-        const exposantName = screen.getByText(name, {exact: false});
-        expect(exposantName).toBeOnTheScreen();
-    })
-}) 
+  it("should display exposant's name as a prop", () => {
+    const name = "Benji la malle";
+    render(<Exposant name={name} />);
+    const exposantName = screen.getByText(name, { exact: false });
+    expect(exposantName).toBeOnTheScreen();
+  });
+
+  it("should display exposant's biography", () => {
+    const biography = "Ceci est une biography sans saut de lignes";
+    render(<Exposant biography={biography} />);
+    const exposantBiography = screen.getByText(biography, { exact: false });
+    expect(exposantBiography).toBeOnTheScreen();
+  });
+
+  it("should display exposant's biography as a prop", () => {
+    const biography = "Ceci est une autre biography sans saut de lignes";
+    render(<Exposant biography={biography} />);
+    const exposantBiography = screen.getByText(biography, { exact: false });
+    expect(exposantBiography).toBeOnTheScreen();
+  });
+});
