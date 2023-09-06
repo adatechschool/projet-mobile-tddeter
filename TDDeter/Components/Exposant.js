a06584a15f41a83603c5a9e5c32faaf38663e6bf
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-09-06 17:04:28 +0200

    [feat] Added formsOfPayment prop to Exposant component

--- TDDeter/Components/Exposant.js
+++ TDDeter/Components/Exposant.js
@@ -1,12 +1,13 @@
 import { Text, View } from "react-native";
 
-export default function Exposant({ name, biography, categories }) {
+export default function Exposant({ name, biography, categories, formsOfPayment }) {
   return (
     <View accessibilityLabel="exposant">
       <Text> Ceci est un brocanteur</Text>
       <Text>Son nom est {name}</Text>
       <Text>Bio : {biography}</Text>
       <Text>Catégories : {categories}</Text>
+      <Text>Moyens de paiement : {formsOfPayment} </Text>
     </View>
   );
 }
