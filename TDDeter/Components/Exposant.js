7b7fea3fa7a90b1b893e4178b4c14e08c56c9701
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-06 16:11:22 +0200

    [feat] Add biography prop to Exposant

--- TDDeter/Components/Exposant.js
+++ TDDeter/Components/Exposant.js
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
 
-export default function Exposant({ name }) {
+export default function Exposant({ name, biography }) {
   return (
     <View accessibilityLabel="exposant">
       <Text> Ceci est un brocanteur</Text>
       <Text>Son nom est {name}</Text>
+      <Text>Bio : {biography}</Text>
     </View>
   );
 }
