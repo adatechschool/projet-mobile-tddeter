d3583038186a5fd58209c1cadc0b930550e22d8a
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-07 16:00:46 +0200

    [test] Test from LoginScreen that when home button of the Footer is pressed, it redirect to HomeScreen

--- TDDeter/tests/Footer.test.js
+++ TDDeter/tests/Footer.test.js
@@ -1,10 +1,26 @@
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, render, screen } from "@testing-library/react-native";
 import Footer from "../Components/Footer";
+import App from "../App";
+import { NavigationContainer } from "@react-navigation/native";
 
 describe("Footer test suite", () => {
   it("should render home button", () => {
-    render(<Footer />);
+    render(
+      <NavigationContainer>
+        <Footer />
+      </NavigationContainer>
+    );
     const homeButton = screen.getByLabelText("accueil", { exact: false });
     expect(homeButton).toBeOnTheScreen();
+  });
+
+  it("should redirect to home screen when home button is pressed from login screen", async () => {
+    render(<App />);
+    const loginButton = screen.getByText("login");
+    fireEvent.press(loginButton);
+    const homeButton = screen.getByLabelText("accueil", { exact: false });
+    fireEvent.press(homeButton);
+    const homeScreen = screen.getByText("Home Screen");
+    expect(homeScreen).toBeOnTheScreen();
   });
 });
