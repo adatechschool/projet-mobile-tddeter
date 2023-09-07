9462894a8c91996b06eb3d0cbeadc1d17ee9dc0c
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-07 11:51:24 +0200

    [feat] change text of button each time showDetails button is pressed

--- TDDeter/Components/Exposant.js
+++ TDDeter/Components/Exposant.js
@@ -1,23 +1,30 @@
 import { Button, Text, View } from "react-native";
 import { useState } from "react";
 
-export default function Exposant({ name, biography, categories, formsOfPayment }) {
-  const [isPressed, setPressed] = useState("Voir +");
+export default function Exposant({
+  name,
+  biography,
+  categories,
+  formsOfPayment,
+}) {
+  const [isPressed, setPressed] = useState("voir +");
 
   const changeTitle = () => {
-    setPressed("Voir -");
+    if (isPressed == "voir +") {
+      setPressed("voir -");
+    } else if (isPressed == "voir -") {
+      setPressed("voir +");
+    }
   };
 
-   return (
+  return (
     <View accessibilityLabel="exposant">
       <Text> Ceci est un brocanteur</Text>
       <Text>Son nom est {name}</Text>
       <Text>Bio : {biography}</Text>
       <Text>Catégories : {categories}</Text>
       <Text>Moyens de paiement : {formsOfPayment} </Text>
-      <Button
-      onPress={changeTitle}
-      title = {isPressed}/>
+      <Button onPress={changeTitle} title={isPressed} />
     </View>
   );
-} 
+}
