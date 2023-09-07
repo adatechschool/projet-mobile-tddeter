5445654eaefca391ffdcdc3e0b52ba4a83c3c733
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-09-07 16:46:43 +0200

    [feat] Handling opacity of elements when showDetails button is pressed

--- TDDeter/Components/Exposant.js
+++ TDDeter/Components/Exposant.js
@@ -8,11 +8,13 @@ export default function Exposant({
   formsOfPayment,
 }) {
   const [isPressed, setPressed] = useState("voir +");
-
+  const [isOpaque, setOpaque] = useState(0);
   const changeTitle = () => {
     if (isPressed == "voir +") {
+      setOpaque(1)
       setPressed("voir -");
     } else if (isPressed == "voir -") {
+      setOpaque(0)
       setPressed("voir +");
     }
   };
@@ -21,10 +23,10 @@ export default function Exposant({
     <View accessibilityLabel="exposant">
       <Text> Ceci est un brocanteur</Text>
       <Text>Son nom est {name}</Text>
-      <Text style={{ opacity: 0}}>Bio : {biography}</Text>
+      <Text style={{ opacity: isOpaque }}>Bio : {biography}</Text>
       <Text>Catégories : {categories}</Text>
-      <Text style={{ opacity: 0}}>Moyens de paiement : {formsOfPayment} </Text>
+      <Text style={{ opacity: isOpaque }}>Moyens de paiement : {formsOfPayment} </Text>
       <Button onPress={changeTitle} title={isPressed} />
-    </View>
+    </View> 
   ); 
 }
