c780f9e153fb83dc459cc5aeb1d98f6ccd31db52
Malena Guallar <malena.guallar@gmail.com>
2023-09-12 12:19:48 +0200

    [feat] Added 'testing' schema in supabase connection

--- TDDeter/backend/database.js
+++ TDDeter/backend/database.js
@@ -2,15 +2,16 @@ import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 require("dotenv").config();
 
-const supabase = createClient(process.env.DB_URL, process.env.DB_API_KEY, {
+const database = createClient(process.env.DB_URL, process.env.DB_API_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
   },
+  db: {
+    schema: "testing",
+  },
 });
 
-module.exports = {
-  supabase,
-};
+module.exports = database;
