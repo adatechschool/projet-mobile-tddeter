f0eb2bd9f4458ab1d5a8efe1b490d14d382220f3
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-13 14:57:33 +0200

    [test] Test NextEvent is displaying brocante data from the database

--- TDDeter/tests/HomeScreen.test.js
+++ TDDeter/tests/HomeScreen.test.js
@@ -2,13 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../Screens/HomeScreen";
 import { screen, render, fireEvent } from "@testing-library/react-native";
 import App from "../App";
+import database from "../backend/database.js";
 
 describe("HomeScreen test suite", () => {
   it("should render next event component", () => {
     render(
       <NavigationContainer>
         <HomeScreen />
-      </NavigationContainer>
+      </NavigationContainer>,
     );
     const NextEvent = screen.getByText("prochain", { exact: false });
     expect(NextEvent).toBeOnTheScreen();
@@ -20,5 +21,24 @@ describe("HomeScreen test suite", () => {
     fireEvent.press(textInfos);
     const BrocanteScreen = screen.getByText("exposant", { exact: false });
     expect(BrocanteScreen).toBeOnTheScreen();
+  });
+
+  it("should display information from brocante table", async () => {
+    const clearDB = await database.from("brocantes").delete().neq("city", "");
+    const city = "Barcelone";
+    const date = "2023-11-08";
+    const { data: dataNewBrocante } = await database
+      .from("brocantes")
+      .insert({ city: city, date: date })
+      .select("id");
+    render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>,
+    );
+    const NextEventCity = screen.findByText(city, { exact: false });
+    const NextEventDate = screen.findByText(date, { exact: false });
+    expect(await NextEventCity).toBeOnTheScreen();
+    expect(await NextEventDate).toBeOnTheScreen();
   });
 });
