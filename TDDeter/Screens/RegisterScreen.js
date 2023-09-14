40d9884d77d1efe4052157937d629c15781d865c
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-14 11:53:48 +0200

    [feat] add email input in register Screen

--- TDDeter/Screens/RegisterScreen.js
+++ TDDeter/Screens/RegisterScreen.js
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 
 export default function RegisterScreen() {
   return (
     <View>
       <Text>Formulaire d'inscription</Text>
+      <TextInput
+        placeholder="Votre Email"
+        // onChangeText={(newMail) => setMail(newMail)}
+      />
     </View>
   );
 }
