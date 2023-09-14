0c8368d2b3ea0f3b69677c1014b3971ae36fb0c0
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-14 12:16:12 +0200

    [test] testing if last name input is displayed in register screen

--- TDDeter/tests/RegisterScreen.test.js
+++ TDDeter/tests/RegisterScreen.test.js
@@ -2,16 +2,23 @@ import RegisterScreen from "../Screens/RegisterScreen";
 import { fireEvent, render, screen } from "@testing-library/react-native";
 
 describe("registerSCreen test suite", () => {
-  it("should display email input", () => {
+  beforeEach(() => {
     render(<RegisterScreen />);
+  });
+  it("should display email input", () => {
     const emailInput = screen.getByPlaceholderText("mail", { exact: false });
     expect(emailInput).toBeOnTheScreen();
   });
   it("should display password input", () => {
-    render(<RegisterScreen />);
     const passwordInput = screen.getByPlaceholderText("passe", {
       exact: false,
     });
     expect(passwordInput).toBeOnTheScreen();
+  });
+  it("should display name input", () => {
+    const nameInput = screen.getByPlaceholderText("nom", {
+      exact: false,
+    });
+    expect(nameInput).toBeOnTheScreen();
   });
 });
