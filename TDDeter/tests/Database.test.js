0fa7311424d04fe4b676fafb0f01bb99f5094802
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-09-14 16:42:29 +0200

    [test] test possibility to insert data in exposantes table

--- TDDeter/tests/Database.test.js
+++ TDDeter/tests/Database.test.js
@@ -1,6 +1,13 @@
 import database from "../backend/database.js";
 
 describe("Database test suite", () => {
+  beforeEach(async () => {
+    const clearDB = await database
+      .from("exposantes")
+      .delete()
+      .neq("first_name", "");
+  });
+
   it("should connect to database", async () => {
     const client = await database;
     expect(client).toBeTruthy();
@@ -13,5 +20,22 @@ describe("Database test suite", () => {
   it("should contain 'exposantes' table and receive 200 code when performing select on table", async () => {
     const response = await database.from("exposantes").select();
     expect(response.status).toBe(200);
+  });
+
+  it("should allow to insert data in exposantes table with statusText created", async () => {
+    const lastName = "Noël";
+    const firstName = "Père";
+    const email = "santa@hoho.com";
+    const password = "leslutinsmalins";
+
+    const response = await database
+      .from("exposantes")
+      .insert({
+        last_name: lastName,
+        first_name: firstName,
+        email: email,
+        password: password,
+      });
+    expect(response.statusText).toBe("Created");
   });
 });
