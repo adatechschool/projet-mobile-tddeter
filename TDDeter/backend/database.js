a6265a70e993dafd1b389699a792f0ba09c8e2dc
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-09-18 16:29:40 +0200

    [build] added react-native-dotenv package

--- TDDeter/backend/database.js
+++ TDDeter/backend/database.js
@@ -1,22 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-require("dotenv").config();
 import "react-native-url-polyfill/auto";
+import { EXPO_PUBLIC_DB_URL, EXPO_PUBLIC_DB_API_KEY } from "@env";
 
-const database = createClient(
-  process.env.EXPO_PUBLIC_DB_URL,
-  process.env.EXPO_PUBLIC_DB_API_KEY,
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-    db: {
-      schema: "testing",
-    },
+const database = createClient(EXPO_PUBLIC_DB_URL, EXPO_PUBLIC_DB_API_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
   },
-);
+  db: {
+    schema: "testing",
+  },
+});
 
 module.exports = database;
