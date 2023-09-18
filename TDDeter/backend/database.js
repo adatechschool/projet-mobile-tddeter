4da7ca4d0387a9d879b59729fa87424c248fb404
Malena Guallar <malena.guallar@gmail.com>
2023-09-18 14:54:16 +0200

    [build] Added React Native Polyfill package for communication between Expo and Supabase

--- TDDeter/backend/database.js
+++ TDDeter/backend/database.js
@@ -1,17 +1,22 @@
 import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 require("dotenv").config();
+import "react-native-url-polyfill/auto";
 
-const database = createClient(process.env.DB_URL, process.env.DB_API_KEY, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
+const database = createClient(
+  process.env.EXPO_PUBLIC_DB_URL,
+  process.env.EXPO_PUBLIC_DB_API_KEY,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+    db: {
+      schema: "testing",
+    },
   },
-  db: {
-    schema: "testing",
-  },
-});
+);
 
 module.exports = database;
